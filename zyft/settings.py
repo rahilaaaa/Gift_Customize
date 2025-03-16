94413b46e24b5e960ef0fb32c93aa9c6d3fcba35
rahila <149465544+rahilaaaa@users.noreply.github.com>
2025-03-16 21:18:06 +0530

    Update settings.py

--- zyft/settings.py
+++ zyft/settings.py
@@ -183,9 +183,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'staticfiles'),  # ✅ Correct because it does NOT conflict with STATIC_ROOT
- ]
 
 
 
